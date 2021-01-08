5a5264cb883284ec5410a57411849155fcd81aef
Hachi <tanaka20180701@gmail.com>
2021-01-08 17:19:59 +0900

    初コミット

---
+++ Python_apprication.py
@@ -0,0 +1,28 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+#ルーティングの指定　---
+@app.route("/")
+def index():
+    return """
+        <html><body>
+        <form action = "/hello" method ="GET">
+          名前:<input type ="text" name="name">
+          <input type ="submit" value="送信">
+        </form>
+        </body></html>
+    """
+
+@app.route("/hello")
+def hello():
+    name=request.args.get("name")
+    if name is None:
+        name="名無し"
+    return """
+    <h1>{0}さん、こんにちは！</h1>
+    """.format(name)
+
+
+#実行
+if __name__=="__main__":
+    app.run(host="127.0.0.1")
