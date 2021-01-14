e7daa89bf6ea9ee272e8972e2569ed3703136d32
Hachi-8 <77132900+Hachi-8@users.noreply.github.com>
2021-01-14 23:40:21 +0900

    調整

--- youtube_scraping.py
+++ youtube_scraping.py
@@ -4,7 +4,7 @@ from apiclient.discovery import build
 import pandas as pd
 import json
 
-def youtube_search(name):
+def youtube_search(word):
     YOUTUBE_API_KEY = 'AIzaSyBPme7scNi_jfFz5cK9rPoSrX68H5-2G5c'
     YOUTUBE_API_SERVICE_NAME = 'youtube'
     YOUTUBE_API_VERSION = 'v3'
@@ -18,13 +18,13 @@ def youtube_search(name):
     search_response = youtube.search().list(
     part='snippet',
     #検索したいワードの指定
-    q='name',
+    q=word,
     #視聴回数が多い順に取得
     order='viewCount',
     type='video',
     ).execute()
 
-    return json.dumps(search_response)
+    return json.dumps(search_response,indent=2,ensure_ascii=False)
 
 def picking_title(arg):
     title=[]
