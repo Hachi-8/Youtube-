5e63b8c17f3ef9160f0f4a8fe0a163a947504103
Hachi-8 <77132900+Hachi-8@users.noreply.github.com>
2021-01-21 19:08:02 +0900

    調整

--- youtube_scraping.py
+++ youtube_scraping.py
@@ -47,14 +47,14 @@ def video_info(video_ids):
     key = " YOUTUBE_API_KEY ",
     id = ",".join(video_ids),
     part = "snippet,contentDetails",
-    maxrusult = 20    
+    maxResult = 20    
     )
 
     video_params = {
         "key" : " YOUTUBE_API_KEY ",
         "id" : ",".join(video_ids),
         "part" : "snippet,contentDetails",
-        "maxrusult" : 20
+        "maxResult" : 20
     }
     
     #r = requests.get(video_url, params=video_params)
