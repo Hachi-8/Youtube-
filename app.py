779608798e7499200a12c4a6a3ac38823efcc55a
Hachi-8 <77132900+Hachi-8@users.noreply.github.com>
2021-02-05 18:39:12 +0900

    調整

--- app.py
+++ app.py
@@ -2,17 +2,46 @@ from flask import Flask,request,redirect,render_template
 from werkzeug.utils import html
 from datetime import datetime
 from youtube_scraping import youtube_search,picking_title,picking_ids,video_info
-from thread import Article,Thread
+#from thread import Article,Thread
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    db_uri = "sqlite:///test.db"
+    #or os.environ.get('DATABASE_URL') #or "postgresql://localhost/flasknote"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(app)
+    return app
 
-db_uri = "sqlite:///test.db"
-#or os.environ.get('DATABASE_URL') #or "postgresql://localhost/flasknote"
-app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+class Article(db.Model):
+    #__tablename__ = "articles"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pub_date = db.Column(db.DateTime, nullable=False,
+                                default=datetime.utcnow)
+    name = db.Column(db.String(80))
+    #name = db.Column(db.Text(80))
+    article = db.Column(db.Text())
+    thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'), nullable=False)
 
+    def __init__(self, pub_date, name, article, thread_id):
+        self.pub_date = pub_date
+        self.name = name
+        self.article = article
+        self.thread_id = thread_id
+
+
+class Thread(db.Model):
+    #__tablename__ = "threads"
+    id = db.Column(db.Integer, primary_key=True)
+    threadname = db.Column(db.String(80), unique=True)
+    #threadname = db.Column(db.Text(80), unique=True)
+    articles = db.relationship('Article', backref='thread', lazy=True)
+
+    def __init__(self, threadname, articles=[]):
+        self.threadname = threadname
+        self.articles = articles
+        
 #ルーティングの指定　---
 @app.route("/")
 def index(): 
