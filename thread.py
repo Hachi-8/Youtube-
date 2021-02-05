aa280b35b543ae540acde5e349afa133a0336a60
Hachi-8 <77132900+Hachi-8@users.noreply.github.com>
2021-02-05 19:24:44 +0900

    調整

--- thread.py
+++ thread.py
@@ -6,30 +6,4 @@ db = SQLAlchemy()
 def init_db(app):
     db.init_app(app)
 
-class Article(db.Model):
-    #__tablename__ = "articles"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    pub_date = db.Column(db.DateTime, nullable=False,
-                                default=datetime.utcnow)
-    name = db.Column(db.String(80))
-    #name = db.Column(db.Text(80))
-    article = db.Column(db.Text())
-    thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'), nullable=False)
 
-    def __init__(self, pub_date, name, article, thread_id):
-        self.pub_date = pub_date
-        self.name = name
-        self.article = article
-        self.thread_id = thread_id
-
-
-class Thread(db.Model):
-    #__tablename__ = "threads"
-    id = db.Column(db.Integer, primary_key=True)
-    threadname = db.Column(db.String(80), unique=True)
-    #threadname = db.Column(db.Text(80), unique=True)
-    articles = db.relationship('Article', backref='thread', lazy=True)
-
-    def __init__(self, threadname, articles=[]):
-        self.threadname = threadname
-        self.articles = articles
