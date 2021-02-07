30f0cf0fd3cf16a058811c0b66ae1b45f30af95e
Hachi-8 <77132900+Hachi-8@users.noreply.github.com>
2021-02-07 19:54:18 +0900

    調整

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ def create_app():
     #or os.environ.get('DATABASE_URL') #or "postgresql://localhost/flasknote"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     return app
+    
+class Thread(db.Model):
+    __tablename__ = "threads"
+    id = db.Column(db.Integer, primary_key=True)
+    threadname = db.Column(db.String(80), unique=True)
+    articles = db.relationship('Article', backref='thread', lazy=True)
+
+    def __init__(self, threadname, articles=[]):
+        self.threadname = threadname
+        self.articles = articles
 
 class Article(db.Model):
     __tablename__ = "articles"
@@ -19,7 +29,6 @@ class Article(db.Model):
     pub_date = db.Column(db.DateTime, nullable=False,
                                 default=datetime.utcnow())
     name = db.Column(db.String(80))
-    #name = db.Column(db.Text(80))
     article = db.Column(db.Text())
     thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'), nullable=False)
 
@@ -29,17 +38,6 @@ class Article(db.Model):
         self.article = article
         self.thread_id = thread_id
 
-
-class Thread(db.Model):
-    __tablename__ = "threads"
-    id = db.Column(db.Integer, primary_key=True)
-    threadname = db.Column(db.String(80), unique=True)
-    #threadname = db.Column(db.Text(80), unique=True)
-    articles = db.relationship('Article', backref='thread', lazy=True)
-
-    def __init__(self, threadname, articles=[]):
-        self.threadname = threadname
-        self.articles = articles
 
 app=create_app()
 #ルーティングの指定　---
