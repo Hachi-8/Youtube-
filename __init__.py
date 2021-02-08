731f6856a3b2c12a0b60607a8d45755a526c4572
Hachi-8 <77132900+Hachi-8@users.noreply.github.com>
2021-02-08 15:19:02 +0900

    調整

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-app.config.from_object('youtube_portal.config') 
+app.config.from_object('config') 
 
 db = SQLAlchemy(app)
 
-import youtube_portal.app
+import app
