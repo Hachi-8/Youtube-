867c00d8b61ad2fb11e762385cfdb2b7af9fce70
Hachi-8 <77132900+Hachi-8@users.noreply.github.com>
2021-02-08 18:36:30 +0900

    調整

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config') 
-    db = SQLAlchemy(app)
-
     return app
+
+
+db = SQLAlchemy(app)
