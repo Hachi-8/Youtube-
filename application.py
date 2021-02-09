ff2806794b73b6bc6985d7ebb1e6869512890cd6
Hachi-8 <77132900+Hachi-8@users.noreply.github.com>
2021-02-09 21:05:14 +0900

    調整

--- application.py
+++ application.py
@@ -2,15 +2,44 @@ from flask import Flask,request,redirect,render_template
 from werkzeug.utils import html
 from youtube_scraping import youtube_search,picking_title,picking_ids,video_info
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 #from __init__ import app
-
+#from model import Article,Thread,db  
 
 app = Flask(__name__)
-app.config.from_object('config') 
 
-from model import Article,Thread,db  
-   
+app.config['SECRET_KEY'] = 'cfb33786023cc152019e747a051f73c6'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db  = SQLAlchemy(app)
+db.create_all()
+
+class Thread(db.Model):
+    #__tablename__ = "threads"
+    id = db.Column(db.Integer, primary_key=True)
+    threadname = db.Column(db.String(80), unique=True)
+    articles = db.relationship('Article', backref='thread', lazy=True)
+
+    def __init__(self, threadname, articles=[]):
+        self.threadname = threadname
+        self.articles = articles
+
+class Article(db.Model):
+    #__tablename__ = "articles"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pub_date = db.Column(db.DateTime, nullable=False,
+                                default=datetime.utcnow())
+    name = db.Column(db.String(80))
+    article = db.Column(db.Text())
+    thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'), nullable=False)
+
+    def __init__(self, pub_date, name, article, thread_id):
+        self.pub_date = pub_date
+        self.name = name
+        self.article = article
+        self.thread_id = thread_id
+
+
 #ルーティングの指定　---
 @app.route("/")
 def index(): 
